ce5af12a8299a09bf8a11b7e64bc63c90763f5fe
Christelle <christelle.lopes92@gmail.com>
2025-08-08 13:04:29 +0200

    first commit

---
+++ event-connect-backend/event-connect-backend/src/main/java/co/simplon/cda/event_connect_backend/EventConnectBackendApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.cda.event_connect_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventConnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventConnectBackendApplication.class, args);
+	}
+
+}
---
+++ event-connect-backend/event-connect-backend/src/test/java/co/simplon/cda/event_connect_backend/EventConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.cda.event_connect_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventConnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
